329aa56c59b029f0d47655ec5678d9f010589696
Ole <106068315+MrGBear@users.noreply.github.com>
2026-03-26 07:50:10 +0000

    Add jdtls Java LSP server

--- init.lua
+++ init.lua
@@ -703,7 +703,9 @@ do
     -- But for many setups, the LSP (`ts_ls`) will work just fine
     -- ts_ls = {},
 
-    stylua = {}, -- Used to format Lua code
+        jdtls = {},
+
+        stylua = {}, -- Used to format Lua code
 
     -- Special Lua Config, as recommended by neovim help docs
     lua_ls = {
