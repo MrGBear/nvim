ad7d7cd16389c913bd38163606b3a781ad4f27fc
Ole <ole.lemnitzer@telekom.de>
2026-03-31 11:02:21 +0000

    Add Undotree

---
+++ lua/custom/plugins/undotree.lua
@@ -0,0 +1,10 @@
+-- Undotree - visualize undo history
+-- https://github.com/jiaoshijie/undotree
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  'jiaoshijie/undotree',
+  cmd = 'UndotreeToggle',
+  opts = {},
+}
