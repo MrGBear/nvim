4b3d4efc47d08ba6ec65a3c8c9f444f014b0e89e
Ole <106068315+MrGBear@users.noreply.github.com>
2026-07-03 18:18:14 +0000

    remove jsonc, as it threw error

--- init.lua
+++ init.lua
@@ -781,7 +781,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'json', 'jsonc', 'yaml', 'nix' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'json', 'yaml', 'nix' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
